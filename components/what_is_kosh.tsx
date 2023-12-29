0f60fc70f7c79ba1206459acb88e02ee76cc9bd7
Ahmad Hassan Ansari <alhaqhassan@gmail.com>
2023-12-29 20:42:36 +0530

    quick fix

--- components/what_is_kosh.tsx
+++ components/what_is_kosh.tsx
@@ -1,9 +1,9 @@
 export default function WhatIsKosh() {
 	return (
 		<div id="what_is_kosh" className="bg-[#C4E7FF33] py-20 px-8">
-			<div className=" mx-auto flex flex-col gap-8 items-center justify-center md:w-[861px]">
+			<div className=" mx-auto flex flex-col gap-8 items-center justify-center md:max-w-[861px]">
 				<h2 className="text-[#014A4A]">What is Koshance?</h2>
-				<p className="text-[#001616]">
+				<p className="text-[#001616] ">
 					Koshance is a crypto finance management platform. We secure your
 					assets while ensuring accessibility for daily operations. With
 					MPC-based wallets, we offer robust security and smooth account
