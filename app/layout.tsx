b5bdf7e7ea3d425b80c67157747c4f1a47f07cf5
Ahmad Hassan Ansari <alhaqhassan@gmail.com>
2024-01-10 01:43:10 +0530

    update: update twk lausanne font

--- app/layout.tsx
+++ app/layout.tsx
@@ -15,12 +15,6 @@ export default function RootLayout({
 }) {
 	return (
 		<html lang="en">
-			<Head>
-				<link
-					href="https://fonts.googleapis.com/css2?family=Roboto:wght@100;300;400;500;700;900&display=swap"
-					rel="stylesheet"
-				/>
-			</Head>
 			<link rel="icon" type="image/svg+xml" href="/icons/favicon.svg" />
 
 			<body>{children}</body>
